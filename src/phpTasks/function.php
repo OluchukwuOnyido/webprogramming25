9fdcf06847a6cc620fb66b9c08bdb6e8e35e870b
OluchukwuOnyido <youronyidooluchukwu@gmail.com-email@example.com>
2026-02-12 14:56:30 +0200

    Add phpTasks files

---
+++ src/phpTasks/function.php
@@ -0,0 +1,21 @@
+<?php
+
+function add($name)
+{
+    echo "$name <br />";
+}
+
+add("Juha");
+add("Oluchukwu");
+
+function add1($name, $address)  {
+    
+echo "Name :$name Address: $address <br />";
+}
+
+
+add1("Oluchukwu","Hämeenlinna");
+add1("Juha" , "Tampere");
+
+
+?>
